42199172ec3c9382fa19e5302cdc8c328776a0e8
Jamiewreed <vintageanalysis@gmail.com>
2018-05-27 14:43:09 -0500

    Update web.py

--- web.py
+++ web.py
@@ -4,22 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  s = sched.scheduler(time.time, time.sleep)
-
-def quote_gen_exe(sc):
-  
-from PIL import Image
-import os, random
-
-    folder="D:\\Jamie\\Python Projects\\Quote Gen\\Image quote gen\\Images"
-
-    a=random.choice(os.listdir(folder))
-    print(a)
-
-    file=folder + '\\' + a
-    Image.open(file).show()
-
-    s.enter(15, 1, quote_gen_exe, (sc,))
-
-s.enter(15, 1, quote_gen_exe, (s,))
-s.run()
+  return 'hello world'
